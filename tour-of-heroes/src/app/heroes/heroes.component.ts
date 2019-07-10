562d7b4c28faacda2aa762f5835947b9779b22f7
eugenio-guarino <eugenio.guarino@students.plymouth.ac.uk>
2019-07-10 20:37:51 +0100

    Revise hero component class

--- tour-of-heroes/src/app/heroes/heroes.component.ts
+++ tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Hero} from '../hero';
+
 
 @Component({
   selector: 'app-heroes',
@@ -7,8 +9,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroesComponent implements OnInit {
 
-  hero = 'Windstorm';
-
+  hero :  Hero ={
+    id: 1,
+    name: 'Windstorm'
+  };
+  
   constructor() { }
 
   ngOnInit() {
