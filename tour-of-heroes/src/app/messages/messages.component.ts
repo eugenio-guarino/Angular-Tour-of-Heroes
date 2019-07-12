d7caa53fe33644a1291891c98ceed85159c1f138
eugenio-guarino <eugenio.guarino@students.plymouth.ac.uk>
2019-07-12 21:04:43 +0100

    Implement feature to add and clear received messages

--- tour-of-heroes/src/app/messages/messages.component.ts
+++ tour-of-heroes/src/app/messages/messages.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {MessagesService} from '../messages.service';
 
 @Component({
   selector: 'app-messages',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MessagesComponent implements OnInit {
 
-  constructor() { }
+  constructor(public messagesService: MessagesService) { }
 
   ngOnInit() {
   }
