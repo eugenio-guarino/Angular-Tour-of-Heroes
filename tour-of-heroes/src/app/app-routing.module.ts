2e802968f767fbe40a8348f198f0a72bc8e5029e
eugenio-guarino <eugenio.guarino@students.plymouth.ac.uk>
2019-07-13 11:38:28 +0100

    Add Heroes Component route

--- tour-of-heroes/src/app/app-routing.module.ts
+++ tour-of-heroes/src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {RouterModule, Routes} from '@angular/router';
+import {HeroesComponent} from './heroes/heroes.component';
 
-
+const routes: Routes = [
+  { path: 'heroes', component: HeroesComponent }
+];
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
