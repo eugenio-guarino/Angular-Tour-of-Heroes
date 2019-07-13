f1f1c4f3982d963dd7be9e1ba5832007501a6c3f
eugenio-guarino <eugenio.guarino@students.plymouth.ac.uk>
2019-07-13 19:43:10 +0100

    Get heroes with HttpClient

--- tour-of-heroes/src/app/app.module.ts
+++ tour-of-heroes/src/app/app.module.ts
@@ -1,8 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule} from '@angular/forms'; //NgModel lives here
-
-import { HttpClientModule } from '@angular/common/http';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
@@ -10,7 +9,11 @@ import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
 import { AppRoutingModule } from './app-routing.module';
 
-import { DashboardComponent } from './dashboard/dashboard.component';
+// Web Api imports
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
+
 
 @NgModule({
   declarations: [
@@ -24,7 +27,14 @@ import { DashboardComponent } from './dashboard/dashboard.component';
     BrowserModule,
     FormsModule,
     AppRoutingModule, 
-    HttpClientModule
+    HttpClientModule,
+
+    // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
+    // and returns simulated server responses.
+    // Remove it when a real server is ready to receive requests
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, { dataEncapsulation: false}
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
--- tour-of-heroes/src/app/hero.service.ts
+++ tour-of-heroes/src/app/hero.service.ts
@@ -4,16 +4,28 @@ import { HEROES } from './mock-heroes';
 import { Observable, of} from 'rxjs';
 import { MessagesService}  from './messages.service';
 
+// Http symbols
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor(private messagesService: MessagesService) { }
+  private log(message: string){
+    this.messagesService.add(`HeroService: ${message}`);
+  }
 
+  private heroesUrl = 'apiheroes'; // URL to web api
+
+  constructor(
+    private http: HttpClient,
+    private messagesService: MessagesService) { }
+
+  /** GET Heroes from the server*/
   getHeroes(): Observable<Hero[]> {
     // TODO: send the message_after_fetching the heroes
-    this.messagesService.add('HeroService: fetched heroes');
+    this.http.get<Hero[]>(this.heroesUrl)
     return of (HEROES);
   }
 
