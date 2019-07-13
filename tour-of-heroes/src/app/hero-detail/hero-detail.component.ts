332ba6e5b697a5bbbef2310ec1bd01e5ac2312e7
eugenio-guarino <eugenio.guarino@students.plymouth.ac.uk>
2019-07-13 14:29:32 +0100

    Add button to go back from hero detail view

--- tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
+++ tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
@@ -28,4 +28,8 @@ export class HeroDetailComponent implements OnInit {
     this.heroService.getHero(id)
       .subscribe(hero => this.hero = hero);
   }
+
+  goBack(): void{
+    this.location.back();
+  }
 }
