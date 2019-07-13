456c8611b6a3769044cc4ecc6ddf254f9e1ec7ab
eugenio-guarino <eugenio.guarino@students.plymouth.ac.uk>
2019-07-13 12:08:08 +0100

    Change dashboard.component.ts default template

--- tour-of-heroes/src/app/dashboard/dashboard.component.ts
+++ tour-of-heroes/src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  heroes: Hero[] = [];
+
+  constructor( private heroService: HeroService) { }
 
   ngOnInit() {
+    this.getHeroes();
   }
 
+  getHeroes(): void{
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes.slice(1,5));
+  }
 }
